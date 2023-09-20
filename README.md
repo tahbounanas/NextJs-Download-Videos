f95408d236952354f4b9504b019aaca3dca6bf8a
Tahboun <145578490+tahbounanas@users.noreply.github.com>
2023-09-20 17:04:45 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# NextJs-Download-Videos
-NextJs-Download-Videos
+# nextjs.videoDownloader
+Multi-site video/audio downloader , mp3/mp4 formats.
+
+next.js + Flask API + SQLAlchemy
+
+- Youtube/Youtube Music/Youtube Shorts
+- Insta reels/Insta videos
+- TikToks/Twitter/Twitch clips/Reddit videos
+
+<p align="center">
+  <img src="https://i.imgur.com/lNqFxhp.jpeg" style="width:100%" />
+</p>
---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.tiktokcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.tiktok**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.twitch**",
+      },
+      {
+        protocol: "https",
+        hostname: "**instagram**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.redd.it",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
